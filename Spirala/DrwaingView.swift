b29cef4a197de99d768a0485555cd5c6e7482ccb
JBo123 <jakub.klucky05@gmail.com>
2023-09-08 21:27:51 +0200

    HomeScreen, basic drawing
    tryna find out how Path() works

---
+++ Spirala/DrwaingView.swift
@@ -0,0 +1,20 @@
+//
+//  DrwaingView.swift
+//  Spirala
+//
+//  Created by Jakub Klucký on 08.09.2023.
+//
+
+import SwiftUI
+
+struct DrwaingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DrwaingView_Previews: PreviewProvider {
+    static var previews: some View {
+        DrwaingView()
+    }
+}
